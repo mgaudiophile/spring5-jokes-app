79996c6dad3356c23ed1f040d9aebb9d24357efd
mgaudiophile <mgaudiofile.git@gmail.com>
2019-11-20 15:56:34 -0800

    Added Services and Controllers.

---
+++ src/main/java/mgaudiofile/spring/joke/jokeapp/controllers/JokesController.java
@@ -0,0 +1,25 @@
+package mgaudiofile.spring.joke.jokeapp.controllers;
+
+import mgaudiofile.spring.joke.jokeapp.services.JokesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private JokesService jokesService;
+
+    @Autowired
+    public JokesController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokesService.getJoke());
+
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/mgaudiofile/spring/joke/jokeapp/services/JokesService.java
@@ -0,0 +1,6 @@
+package mgaudiofile.spring.joke.jokeapp.services;
+
+public interface JokesService {
+
+    String getJoke();
+}
